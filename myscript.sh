9cf3ab08da63aa9da2a720b7f924073c3af26b25
Thirunavukkarasu Arumugam <thirunavukkarasu.arumugam@oracle.com>
2022-07-19 13:29:31 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -3,18 +3,18 @@
 TESTDIR=~/testdir
 TESTFILE=~/testdir/testout.txt
 
-runuser -l wlsqa -c "mkdir -p $TESTDIR"
-runuser -l wlsqa -c "rm -rf $TESTFILE"
+runuser -l oracle -c "mkdir -p $TESTDIR"
+runuser -l oracle -c "rm -rf $TESTFILE"
 
-runuser -l wlsqa -c "echo 'java -version' &>> $TESTFILE"
-runuser -l wlsqa -c "java -version &>> $TESTFILE"
+runuser -l oracle -c "echo 'java -version' &>> $TESTFILE"
+runuser -l oracle -c "java -version &>> $TESTFILE"
 
-runuser -l wlsqa -c "echo 'ifconfig' &>> $TESTFILE"
-runuser -l wlsqa -c "ifconfig &>> $TESTFILE"
+runuser -l oracle -c "echo 'ifconfig' &>> $TESTFILE"
+runuser -l oracle -c "ifconfig &>> $TESTFILE"
 
-runuser -l wlsqa -c "echo 'hostname: ' &>> $TESTFILE"
-runuser -l wlsqa -c "hostname -f  &>> $TESTFILE"
+runuser -l oracle -c "echo 'hostname: ' &>> $TESTFILE"
+runuser -l oracle -c "hostname -f  &>> $TESTFILE"
 
-runuser -l wlsqa -c "echo 'Script execution is completed.'  &>> $TESTFILE"
+runuser -l oracle -c "echo 'Script execution is completed.'  &>> $TESTFILE"
 
-runuser -l wlsqa -c "cat $TESTFILE"
+runuser -l oracle -c "cat $TESTFILE"
