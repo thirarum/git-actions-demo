1e6d43fe7285b9bfc657c6e29794d0b130e83f4d
Thirunavukkarasu Arumugam <thirunavukkarasu.arumugam@oracle.com>
2022-08-01 18:21:17 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-export USER_HOME=/home/oracle
+resourceGroup=$1
+clusterName=$2
 
-wget https://raw.githubusercontent.com/thirarum/git-actions-demo/patch-1/myscript.sh
-cp myscript.sh $USER_HOME
-chown oracle:oracle $USER_HOME/myscript.sh
-chmod +x $USER_HOME/myscript.sh
-runuser -l oracle -c ". $USER_HOME/myscript.sh"
+echo "Resource Group: $resourceGroup"
+echo "AKS cluster: $clusterName"
+
+echo "====================="
+echo "Installing Azure CLI"
+echo "====================="
+sudo curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash 
+az --version
+
+echo "==========================="
+echo "Installing kubectl client"
+echo "==========================="
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+echo "==========================="
+echo "Install jq for log parsing"
+echo "==========================="
+sudo apt install jq
+
+echo "================================="
+echo "Obtain KUBECONFIG through az CLI"
+echo "================================="
+az aks get-credentials --resource-group ${resourceGroup} --name ${clusterName} --file ${PWD}/${clusterName}.conf
